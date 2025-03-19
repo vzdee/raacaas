ac4f8091409cbc8510767e33942d2301cb106f71
vzdee <149275740+vzdee@users.noreply.github.com>
2025-03-18 23:21:22 -0600

    update

--- services.php
+++ services.php
@@ -9,7 +9,7 @@ require 'layout/partials/dasheader.php'; // Realizar las consultas SQL
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <<link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Mis Servicios</title>
 </head>
 <body class="mis-servicios-page">
